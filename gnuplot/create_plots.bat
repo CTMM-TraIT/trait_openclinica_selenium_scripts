8d83e21d34dc48b1750e8b5b2b4bf97ca1e78fde
j.rousseau <j.rousseau@vumc.nl>
2015-09-23 13:57:23 +0200

    Final fixes to the generation of the graphs from the SE-scripts which check the performance of openclinica.nl

--- gnuplot/create_plots.bat
+++ gnuplot/create_plots.bat
@@ -7,11 +7,11 @@ REM
 REM ------------------------------------------------------------------------------------
 
 type ..\..\VUmc-ONC-PC306\results-runs.txt | findstr "EditExistingSubject" > EditExistingSubject-VUmc-ONC-PC306.dat
-type ..\..\VUmc-ONC-PC306\results-runs.txt | findstr "ClickingThrough" >ClickingThrough-VUmc-ONC-PC306.dat
+
 
 REM Next lines are for the new extended measurements
 type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Login" > Extended_Login-VUmc-ONC-PC306.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_ClickSubjectMatrix" > Extended_Click_Through_Subject_Matrix-VUmc-ONC-PC306.dat
+REM type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_ClickSubjectMatrix" > Extended_Click_Through_Subject_Matrix-VUmc-ONC-PC306.dat
 type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15.dat
 type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50.dat
 type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section1_Save" > Extended_Section1_Save-VUmc-ONC-PC306.dat
