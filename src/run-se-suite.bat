2b714edb332b67816f0218a3c19879353aedfedf
j.rousseau <j.rousseau@vumc.nl>
2015-11-16 09:59:36 +0100

    Fix in output of the old ClickThrough and EditExistingSubject. The output was not piped correctly to the results.txt file

--- src/run-se-suite.bat
+++ src/run-se-suite.bat
@@ -17,7 +17,7 @@ REM ---------------------------------------------------
 REM %JAVA_EXECUTABLE% -jar selenium-server-standalone-2.44.0.jar -Dwebdriver.firefox.profile=Firefox-profile-selenium-testing -trustAllSSLCertificates -userExtensions %BASE_DIR%user-extensions.js -htmlSuite "*firefox"  https://www.openclinica.nl/ %BASE_DIR%Suite-test-performance-prod.html %BASE_DIR%Results.html 
 
 
-java -jar selenium-server-standalone-2.44.0.jar -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox  C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe" https://www-acc.openclinica.nl/ test-performance-suite.html Results.html 
+java -jar selenium-server-standalone-2.47.1.jar -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox  C:\\Documents and Settings\\j.rousseau\\Local Settings\\Application Data\\Mozilla Firefox\\firefox.exe" https://www.openclinica.nl/ test-performance-suite.html Results.html 
 
 
 
@@ -27,7 +27,7 @@ REM This is the Selenium Webdriver variant:
 REM ---------------------------------------------------
 REM %JAVA_EXECUTABLE% -jar selenium-server-standalone-2.44.0.jar -Dwebdriver.firefox.profile=Firefox-profile-selenium-testing -trustAllSSLCertificates -userExtensions %BASE_DIR%user-extensions.js -htmlSuite "*webdriver"  https://www.openclinica.nl/ %BASE_DIR%Suite-test-performance-prod.html %BASE_DIR%Results.html 
 
-findstr  /R "Duration.*[0-9].*" Results.html | findstr successfully | gawk "{print \"%DATE%\t%TIME%\t\"$6,$21}" >> results-runs.txt
+findstr  /R "Duration.*[0-9].*" Results.html | findstr successfully | gawk "{print \"%DATE%\t%TIME%\t\"$6,$17}" >> results-runs.txt
 findstr  /R "_Duration.*[0-9].*" Results.html | gawk "{print $5, $(NF-9), $(NF-8), $(NF-7), $(NF-6), $(NF-5), $(NF-4), $(NF-3), $(NF-2), $(NF-1)}" >> results-runs-extended.txt
 
 
