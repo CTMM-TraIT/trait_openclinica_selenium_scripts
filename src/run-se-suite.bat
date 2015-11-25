2679b523aebe54242b77f0a6a5d6a4913b9cfd70
j.rousseau <j.rousseau@vumc.nl>
2015-11-25 14:00:46 +0100

    Last fixes of the Windows7 setup. Works on my PC

--- src/run-se-suite.bat
+++ src/run-se-suite.bat
@@ -1,3 +1,6 @@
+echo Command-line %~1
+SET FIREFOX_PROFILE_NAME=%~1
+echo Firefox executable %FIREFOX_PROFILE_NAME%
 @ECHO OFF
 
 REM
@@ -14,12 +17,17 @@ REM ---------------------------------------------------
 
 if "%1"=="" goto BLANK
 
-java -jar selenium-server-standalone-2.47.1.jar -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox %1" https://www.openclinica.nl/ test-performance-suite.html Results.html 
+java -jar selenium-server-standalone-2.44.0.jar -Dwebdriver.firefox.profile=%FIREFOX_PROFILE_NAME% -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox" https://www.openclinica.nl/ test-performance-suite.html Results.html 
 
 REM ---------------------------------------------------
-REM This is the Selenium Webdriver variant: 
+REM The date format of selenium is not uniform over different Windows versions.
+REM If the last entry in a row in the results-runs.txt does not contain a number the you have to change the value of the 
+REM second parameter below from $%17 to the appropriate value.
+REM You can check it by running the command: >> findstr  /R "Duration.*[0-9].*" Results.html | findstr successfully << 
+REM For example the output could be:
+REM "<td>Result of test run: test ClickingThrough completed successfully at Wed Nov 25 2015 13:25:16 GMT+0100 (W. Europe Standard Time). Duration 63854 ms</td>"
+REM In this case the $17 should be $21; just count the number of tokens separated by a space.
 REM ---------------------------------------------------
-REM %JAVA_EXECUTABLE% -jar selenium-server-standalone-2.44.0.jar -Dwebdriver.firefox.profile=Firefox-profile-selenium-testing -trustAllSSLCertificates -userExtensions %BASE_DIR%user-extensions.js -htmlSuite "*webdriver"  https://www.openclinica.nl/ %BASE_DIR%Suite-test-performance-prod.html %BASE_DIR%Results.html 
 
 findstr  /R "Duration.*[0-9].*" Results.html | findstr successfully | gawk "{print \"%DATE%\t%TIME%\t\"$6,$17}" >> results-runs.txt
 findstr  /R "_Duration.*[0-9].*" Results.html | gawk "{print $5, $(NF-9), $(NF-8), $(NF-7), $(NF-6), $(NF-5), $(NF-4), $(NF-3), $(NF-2), $(NF-1)}" >> results-runs-extended.txt
