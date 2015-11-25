1303ab3f0d50482a22117e12f03a8799bc476f2d
j.rousseau <j.rousseau@vumc.nl>
2015-11-25 11:41:26 +0100

    Work on getting the scripts working on Windows 7

--- src/run-se-suite.bat
+++ src/run-se-suite.bat
@@ -8,19 +8,13 @@ REM -userExtensions C:\_home\jacob\selenium-contin\user-extensions.js
 REM -firefoxProfileTemplate C:\_home\jacob\selenium-contin\firefox-profile
 
 
-REM set JAVA_EXECUTABLE=..\java1.6.0.45\bin\java
-
-set JAVA_EXECUTABLE="C:\java\jdk1.7.0_51\bin\java"
 REM ---------------------------------------------------
 REM This is the Firefox variant:
 REM ---------------------------------------------------
-REM %JAVA_EXECUTABLE% -jar selenium-server-standalone-2.44.0.jar -Dwebdriver.firefox.profile=Firefox-profile-selenium-testing -trustAllSSLCertificates -userExtensions %BASE_DIR%user-extensions.js -htmlSuite "*firefox"  https://www.openclinica.nl/ %BASE_DIR%Suite-test-performance-prod.html %BASE_DIR%Results.html 
 
+if "%1"=="" goto BLANK
 
-java -jar selenium-server-standalone-2.47.1.jar -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox  C:\\Documents and Settings\\j.rousseau\\Local Settings\\Application Data\\Mozilla Firefox\\firefox.exe" https://www.openclinica.nl/ test-performance-suite.html Results.html 
-
-
-
+java -jar selenium-server-standalone-2.47.1.jar -trustAllSSLCertificates -avoidProxy -userExtensions user-extensions.js -htmlSuite "*firefox %1" https://www.openclinica.nl/ test-performance-suite.html Results.html 
 
 REM ---------------------------------------------------
 REM This is the Selenium Webdriver variant: 
@@ -31,7 +25,13 @@ findstr  /R "Duration.*[0-9].*" Results.html | findstr successfully | gawk "{pri
 findstr  /R "_Duration.*[0-9].*" Results.html | gawk "{print $5, $(NF-9), $(NF-8), $(NF-7), $(NF-6), $(NF-5), $(NF-4), $(NF-3), $(NF-2), $(NF-1)}" >> results-runs-extended.txt
 
 
-copy /Y results-runs.txt O:\Pub\dm-algemeen\CTMS\TraIT\WP1\performance-monitoring\VUmc-ONC-PC306
-copy /Y results-runs-extended.txt O:\Pub\dm-algemeen\CTMS\TraIT\WP1\performance-monitoring\VUmc-ONC-PC306
+REM copy /Y results-runs.txt O:\Pub\dm-algemeen\CTMS\TraIT\WP1\performance-monitoring\VUmc-ONC-PC306
+REM copy /Y results-runs-extended.txt O:\Pub\dm-algemeen\CTMS\TraIT\WP1\performance-monitoring\VUmc-ONC-PC306
 ECHO Finished
+GOTO DONE
+
+:BLANK
+echo Firefox profile directory missing.
+
+:DONE
 @ECHO ON
