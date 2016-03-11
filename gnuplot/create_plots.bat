e5943d9bce95abbbf9605c4153d4265d6f7b1464
Jacob Rousseau <j.rousseau@nki.nl>
2016-03-11 16:55:44 +0100

    Update of the scripts to be able to run at the NKI. The create plots batch file now includes 3 parameters: the directory where the results of the SE-script can be found, an ID for the host where the data is collected and a date/time indicator. The last 2 parameter are added to the titles of the graphs. $

--- gnuplot/create_plots.bat
+++ gnuplot/create_plots.bat
@@ -1,4 +1,10 @@
-@echo off
+@ECHO OFF
+
+if "%1"=="" goto BLANK
+
+SET OUTPUT_DIR=%~1
+SET HOST=%~2
+SET START_DATE=%3
 REM ------------------------------------------------------------------------------------
 REM
 REM Batch file to create a plot/graph of the duration of the runs of the performance 
@@ -7,15 +13,27 @@ REM
 REM ------------------------------------------------------------------------------------
 
 
-type ..\..\VUmc-ONC-PC306\results-runs.txt | findstr "EditExistingSubject" > EditExistingSubject-VUmc-ONC-PC306.dat
+
+
+type %OUTPUT_DIR%\results-runs.txt | findstr "EditExistingSubject" > EditExistingSubject-%HOST%.dat
 
 
 REM Next lines are for the new extended measurements
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Login" > Extended_Login-VUmc-ONC-PC306.dat
-REM type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_ClickSubjectMatrix" > Extended_Click_Through_Subject_Matrix-VUmc-ONC-PC306.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section1_Save" > Extended_Section1_Save-VUmc-ONC-PC306.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section2_Save" > Extended_Section2_Save-VUmc-ONC-PC306.dat
-type ..\..\VUmc-ONC-PC306\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section3_Save" > Extended_Section3_Save-VUmc-ONC-PC306.dat
-gnuplot gnuplot.script
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Login" > Extended_Login-%HOST%.dat
+REM type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_ClickSubjectMatrix" > Extended_Click_Through_Subject_Matrix-%HOST%.dat
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_15.dat
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50" > Extended_Edit_Existing_Subject_-_LoadSubjectMatrix_size_50.dat
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section1_Save" > Extended_Section1_Save-%HOST%.dat
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section2_Save" > Extended_Section2_Save-%HOST%.dat
+type %OUTPUT_DIR%\results-runs-extended.txt | findstr "Extended_Edit_Existing_Subject_-_Section3_Save" > Extended_Section3_Save-%HOST%.dat
+gnuplot -c gnuplot.script %HOST% %START_DATE%
+
+
+ECHO Finished
+GOTO DONE
+
+:BLANK
+echo Output directory missing where the files results-runs.txt and results-runs-extended.txt can be found
+
+:DONE
+@ECHO ON
